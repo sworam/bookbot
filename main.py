393e7305708f46bc8e241f22858990d5eeff9c9f
Marlo Swora <marlo.swora@mail.de>
2025-01-02 16:58:05 +0100

    refactor with statements

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 def main():
+    content = None
     with open("books/frankenstein.txt") as f:
-        word_count = 0
-        for line in f:
-            word_count += len(line.split())
-        print(word_count)
+        content = f.read()
+    word_count = len(content.split())
+    print(word_count)
 
-    with open("books/frankenstein.txt") as f:
-        character_dict = dict()
-        for c in f.read().lower():
-            if c not in character_dict:
-                character_dict[c] = 0
-            character_dict[c] += 1
-        
-        for key in character_dict.keys():
-            print(f"{key}: {character_dict[key]}")
+    character_dict = dict()
+    for c in content.lower():
+        if c not in character_dict:
+            character_dict[c] = 0
+        character_dict[c] += 1
+    
+    for key in character_dict.keys():
+        print(f"{key}: {character_dict[key]}")
 
 if __name__ == "__main__":
     main()
