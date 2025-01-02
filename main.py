942e06a4ecddc6ced9424da4f7c43f8e200adfc4
Marlo Swora <marlo.swora@mail.de>
2025-01-02 16:54:07 +0100

    add word and character count

---
+++ main.py
@@ -0,0 +1,15 @@
+with open("books/frankenstein.txt") as f:
+    word_count = 0
+    for line in f:
+        word_count += len(line.split())
+    print(word_count)
+
+with open("books/frankenstein.txt") as f:
+    character_dict = dict()
+    for c in f.read().lower():
+        if c not in character_dict:
+            character_dict[c] = 0
+        character_dict[c] += 1
+    
+    for key in character_dict.keys():
+        print(f"{key}: {character_dict[key]}")
