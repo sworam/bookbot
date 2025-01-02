f83d40cde94c9bb783e7494903de208aef8ad475
Marlo Swora <marlo.swora@mail.de>
2025-01-02 17:05:33 +0100

    refactor main to independent functions.

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
-def main():
-    content = None
-    with open("books/frankenstein.txt") as f:
-        content = f.read()
-    word_count = len(content.split())
-    print(word_count)
+from collections import defaultdict
 
-    character_dict = dict()
-    for c in content.lower():
+
+def read_book(book: str) -> str:
+    content = None
+    with open(book) as f:
+        content = f.read()
+    return content
+
+
+def calc_word_count(text: str) -> int:
+    return len(text.split())
+
+
+def calc_character_dict(text: str) -> int:
+    character_dict = defaultdict(lambda: 0)
+    for c in text.lower():
         if c not in character_dict:
             character_dict[c] = 0
         character_dict[c] += 1
-    
-    for key in character_dict.keys():
+    return character_dict
+
+
+def main():
+    content = read_book("books/frankenstein.txt")    
+    word_count = calc_word_count(content)
+    print(word_count)
+
+    character_dict = calc_character_dict(content)    
+    for key in character_dict:
         print(f"{key}: {character_dict[key]}")
 
 if __name__ == "__main__":
