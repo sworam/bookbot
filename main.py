2f584315e983b07d54503768a90f3b7bc405644c
Marlo Swora <marlo.swora@mail.de>
2025-01-02 16:56:00 +0100

    refactor main()

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-with open("books/frankenstein.txt") as f:
-    word_count = 0
-    for line in f:
-        word_count += len(line.split())
-    print(word_count)
+def main():
+    with open("books/frankenstein.txt") as f:
+        word_count = 0
+        for line in f:
+            word_count += len(line.split())
+        print(word_count)
 
-with open("books/frankenstein.txt") as f:
-    character_dict = dict()
-    for c in f.read().lower():
-        if c not in character_dict:
-            character_dict[c] = 0
-        character_dict[c] += 1
-    
-    for key in character_dict.keys():
-        print(f"{key}: {character_dict[key]}")
+    with open("books/frankenstein.txt") as f:
+        character_dict = dict()
+        for c in f.read().lower():
+            if c not in character_dict:
+                character_dict[c] = 0
+            character_dict[c] += 1
+        
+        for key in character_dict.keys():
+            print(f"{key}: {character_dict[key]}")
+
+if __name__ == "__main__":
+    main()
